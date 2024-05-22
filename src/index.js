16c89a508f6cba91dba3d0d06f4c72756be34104
Muh. Fitrah Ramadhan <mftrhr@gmail.com>
2024-05-22 10:36:10 +0800

    update code: services

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ const app = express();
 const { connectDB } = require("./db/db");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-const Otp = require("./models/otp.model");
-const User = require("./models/user.model");
 
 //.env
 require("dotenv").config();
@@ -18,29 +16,15 @@ app.use(express.json());
 
 app.use(cookieParser());
 
-//koneksi database
+//connec database
 connectDB();
 
 app.get("/api", (_, r) => {
   r.json({
     status: "OK",
     version: "1.0.0",
-    env: parseInt(process.env.COOKIE_EXP),
   });
 });
-const authMiddleware = require("./middlewares/auth.middleware");
-app.get("/users", authMiddleware, async (_, r) => {
-  r.json({
-    users: await User.find({}),
-    otps: await Otp.find({}),
-  });
-});
-
-const UserRoutes = require("./user/user.router");
-app.use("/", UserRoutes);
-
-const BooksRoutes = require("./books/books.router");
-app.use("/book", BooksRoutes);
 
 const AuthRoutes = require("./auth/auth.router");
 app.use("/auth", AuthRoutes);
